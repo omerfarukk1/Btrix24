67a40ee70aa4ce500f13c91a339e0d6058b42131
omerfarukk1 <54939223+omerfarukk1@users.noreply.github.com>
2019-12-09 17:37:45 +0000

    updated task class

--- src/test/java/tests/Task.java
+++ src/test/java/tests/Task.java
@@ -1,6 +1,13 @@
 package tests;
 
 public class Task {
+  
+  public static void main(String[] args){
+    
+    System.out.println("Hello Java");
+    // this line is added from Github, se you intelliJ
+  
+  }
 
 
 
